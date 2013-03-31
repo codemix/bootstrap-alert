1948abbc88c87f780703fca7fc4b61395f74e3a9
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:35 +0100

    Added tag v2.1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ==========================================================
-   * bootstrap-alert.js v2.1.0
+   * bootstrap-alert.js v2.1.1
    * http://twitter.github.com/bootstrap/javascript.html#alerts
    * ==========================================================
    * Copyright 2012 Twitter, Inc.
